f0aebc8b3183a6079245d47dff42187fd2025525
Rudolph Lorenz <rudlorenz@gmail.com>
2018-08-01 17:43:42 +0700

    ImageGeneration: fix operator() bug. Move function definitions to header

--- ImageGeneration/bitmapimg.h
+++ ImageGeneration/bitmapimg.h
@@ -1,4 +1,32 @@
-#include "bmpgen.h"
+#ifndef BITMAPIMG_H
+#define BITMAPIMG_H
+
+#include "bmpheader.h"
+
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+class BitmapImg
+{
+public:
+    BitmapImg();
+    BitmapImg(unsigned width, int height);
+
+    int writeToFile(const std::string& filename);
+
+    unsigned getWidth() const;
+    int      getHeight() const;
+
+    BGRPalette& operator()(size_t row, size_t column);
+    const BGRPalette& operator()(size_t row, size_t column) const;
+
+private:
+    unsigned width_;
+    int      height_;
+    std::vector<BGRPalette> pixels_;
+};
 
 
 BitmapImg::BitmapImg() : width_(0), height_(0), pixels_(0)
@@ -8,9 +36,9 @@ BitmapImg::BitmapImg() : width_(0), height_(0), pixels_(0)
 
 BitmapImg::BitmapImg(unsigned width, int height) : width_(width), height_(height),
                                                    pixels_(3 * (((4 - (width % 4)) % 4) + width) * std::abs(height), 0)
-                                                   //a bit too complicated maybe? calculations needed to add padding
-                                                   //declared in bmp file format:
-                                                   // "Each row in the Pixel array is padded to a multiple of 4 bytes in size "
+//a bit too complicated maybe? calculations needed to add padding
+//declared in bmp file format:
+// "Each row in the Pixel array is padded to a multiple of 4 bytes in size "
 {
 }
 
@@ -70,14 +98,17 @@ BGRPalette &BitmapImg::operator()(size_t row, size_t column)
     if (row >= std::abs(height_) || column >= width_) { throw std::out_of_range("Index out of bounds"); }
 
     //turn the image upside down so (0,0) is in left top counter
-    return pixels_[(height_ - row) * height_ + column];
+    return pixels_[(height_ - row - 1) * width_ + column];
 }
 
-BGRPalette BitmapImg::operator()(size_t row, size_t column) const
+const BGRPalette &BitmapImg::operator()(size_t row, size_t column) const
 {
+    //don't allow user to access "padded" pixels
     if (row >= std::abs(height_) || column >= width_) { throw std::out_of_range("Index out of bounds"); }
 
     //turn the image upside down so (0,0) is in left top counter
-    return pixels_[(height_ - row) * height_ + column];
+    return pixels_[(height_ - row - 1) * width_ + column];
 }
 
+
+#endif //BITMAPIMG_H
