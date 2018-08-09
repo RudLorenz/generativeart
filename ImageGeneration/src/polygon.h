5919ce1a08d60e01062174294da46bed77dae9ad
Rudolph Lorenz <rudlorenz@gmail.com>
2018-08-10 01:46:37 +0700

    ImageGeneration: fix polygon plotting method

--- ImageGeneration/src/polygon.h
+++ ImageGeneration/src/polygon.h
@@ -20,6 +20,11 @@ public:
 
     void draw(BitmapImg &canvas, const BGRPalette &color) override;
 
+protected:
+    void plotLineLow (const Point& p1, const Point& p2, BitmapImg &canvas, const BGRPalette &color);
+    void plotLineHigh(const Point& p1, const Point& p2, BitmapImg &canvas, const BGRPalette &color);
+    void plotLine    (const Point& p1, const Point& p2, BitmapImg &canvas, const BGRPalette &color);
+
 private:
     std::vector<Point> points_;
 };
@@ -65,15 +70,96 @@ size_t Polygon::size() const
 
 void Polygon::draw(BitmapImg &canvas, const BGRPalette &color)
 {
-    for (unsigned i = 0; i < points_.size()-1; ++i)
+    for (auto it = points_.begin(); it != points_.end()-1; ++it)
     {
-        // bad design. Should probably add a method to Point class to draw line to another point
-        // or create a separate function that will do the job.
-        Line ln(points_[i], points_[i+1]);
-        ln.draw(canvas, color);
+        plotLine(*it, *(it+1), canvas, color);
     }
-    Line ln(points_.front(), points_.back());
-    ln.draw(canvas, color);
+    plotLine(points_.front(), points_.back(), canvas, color);
+}
+
+
+void Polygon::plotLineLow(const Point& p1, const Point& p2, BitmapImg &canvas, const BGRPalette &color)
+{
+    int dx = p2.x() - p1.x();
+    int dy = p2.y() - p1.y();
+
+    int y_sign = 1;
+
+    if (dy < 0)
+    {
+        y_sign = -1;
+        dy = -dy;
+    }
+
+    int approx = 2 * dy - dx;
+    unsigned y_step = p1.y();
+
+    for (unsigned x_step = p1.x(); x_step <= p2.x(); ++x_step)
+    {
+        canvas(x_step, y_step) = color;  // TODO add sign + range check
+
+        if (approx > 0)
+        {
+            y_step = y_step + y_sign;
+            approx = approx - 2 * dx;
+        }
+
+        approx = approx + 2 * dy;
+    }
+}
+
+
+void Polygon::plotLineHigh(const Point& p1, const Point& p2, BitmapImg &canvas, const BGRPalette &color)
+{
+    int dx = p2.x() - p1.x();
+    int dy = p2.y() - p1.y();
+
+    int x_sign = 1;
+
+    if (dx < 0)
+    {
+        x_sign = -1;
+        dx = -dx;
+    }
+
+    int approx = 2 * dx - dy;
+    unsigned x_step = p1.x();
+
+    for (unsigned y_step = p1.y(); y_step <= p2.y(); ++y_step)
+    {
+        canvas(x_step, y_step) = color;  // TODO add sign + range check
+
+        if (approx > 0)
+        {
+            x_step = x_step + x_sign;
+            approx = approx - 2 * dy;
+        }
+
+        approx = approx + 2 * dx;
+    }
+}
+
+
+void Polygon::plotLine(const Point &p1, const Point &p2, BitmapImg &canvas, const BGRPalette &color)
+{
+    if (std::abs(static_cast<int>(p2.y() - p1.y())) < std::abs(static_cast<int>(p2.x() - p1.x())))
+    {
+        if (p1.x() > p2.x()) {
+            plotLineLow(p2, p1, canvas, color);
+        }
+        else {
+            plotLineLow(p1, p2, canvas, color);
+        }
+    }
+    else
+    {
+        if (p1.y() > p2.y()) {
+            plotLineHigh(p2, p1, canvas, color);
+        }
+        else {
+            plotLineHigh(p1, p2, canvas, color);
+        }
+    }
 }
 
 #endif // POLYGON_H
